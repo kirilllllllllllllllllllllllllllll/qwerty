98bb8b5421bc975e75d1a679bd80506a9112dadc
kirilllllllllllllllllllllllllllll <150334314+kirilllllllllllllllllllllllllllll@users.noreply.github.com>
2023-11-09 16:57:55 +0700

    Add files via upload
    commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import sys
+from random import randint
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 300)
+        self.setWindowTitle('Рисование')
+        uic.loadUi('UI.ui', self)
+        self.do_paint = False
+        self.btn.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        num = randint(20, 260)
+        qp.drawEllipse(0, 30, num, num)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
